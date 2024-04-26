93dfb729d508ed2f2f8b687d80f70187053c89a2
akzvitor <vitor_eterno@outlook.com>
2024-04-26 15:36:30 -0300

    implementa o exercício 1 na classe Vetor

---
+++ atividade08/excecoes/src/Vetor.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Vetor {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] array = new int[10];
+        int i = 0;
+        int number;
+
+        try {
+            do {
+                System.out.print("Digite o número a ser inserido no vetor ou 0 para encerrar: ");
+                number = scanner.nextInt();
+                array[i] = number;
+                i++;
+            } while (number != 0);
+
+            System.out.print("Elementos do vetor: ");
+            for (int j = 0; j < i; j++) {
+                System.out.print(array[j] + " ");
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Limite de 10 valores excedido.");
+        } catch (InputMismatchException e) {
+            System.out.println("O valor deve ser um número inteiro.");
+        }
+    }
+}
